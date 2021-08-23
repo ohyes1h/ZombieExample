57ba17eaf6c6fbdf29f9642786ccca1d5884601b
sih.ouyang <sih.ouyang@gogoro.com>
2021-08-23 15:40:52 +0800

    Initial Commit

---
+++ Observation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Observation
+//
+//  Created by Sih Ouyang 歐陽思 on 2021/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
