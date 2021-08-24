37d3881beff0b7ae8929f497672e165d74078821
sih.ouyang <sih.ouyang@gogoro.com>
2021-08-24 15:57:50 +0800

    first commit

---
+++ Observation/Connector.swift
@@ -0,0 +1,8 @@
+//
+//  Connector.swift
+//  Observation
+//
+//  Created by Sih Ouyang 歐陽思 on 2021/8/23.
+//
+
+import Foundation
---
+++ Observation/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  Observation
+//
+//  Created by Sih Ouyang 歐陽思 on 2021/8/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
